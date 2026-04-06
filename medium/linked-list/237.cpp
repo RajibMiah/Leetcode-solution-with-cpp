329f25ca553052f9e7fecb15bf31d0c67a2fef09
Rajib Miah <rajib@Rajibs-MacBook-Air.local>
2026-04-06 20:44:44 +0200

    sovle linked list leedcode 237

---
+++ medium/linked-list/237.cpp
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+
+        prev = ListNode()
+
+        while(node and node.next):
+            prev =  node;
+            prev.val = node.next.val
+            node = node.next
+        
+        prev.next = None
+        
